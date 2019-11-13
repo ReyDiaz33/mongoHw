44135c721e8f5fb1a1cff9f1c0f4e7e9f358c22a
Rey Diaz <reybdiazjr@gmail.com>
2019-11-13 10:42:05 -0500

    Set up mongoose connections and error/success message

--- server.js
+++ server.js
@@ -13,11 +13,22 @@ app.use(
     })
 );
 
+app.use(express.static(process.cwd() + "/public"));
+
 var exphbs = require ("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout: "main"})
 );
 app.set("view engine", "handlebars");
+// mongoose connection
+mongoose.connect("mongodb://localhost/scraped_news");
+var db = mongoose.connection;
+// error message or successfully connected to mongoose
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function() {
+    console.log("Connected to Mongoose!"); 
+});
 
+// set up PORT for 3000
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
     console.log("Listening on PORT " + port);
