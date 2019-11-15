e5504a89533f3a20987215496cdca78143b3d6f5
Rey Diaz <reybdiazjr@gmail.com>
2019-11-15 09:35:06 -0500

    fully functiomal scraper news attached the clearAll and the scraping button to work. Also a button on my nav bar for my git repo.

--- server.js
+++ server.js
@@ -1,40 +1,46 @@
+//dependencies
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var logger = require("morgan");
 
+//initialize Express app
 var express = require("express");
 var app = express();
 
-
-
-// setting up logger
 app.use(logger("dev"));
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
+  bodyParser.urlencoded({
+    extended: false
+  })
 );
 
 app.use(express.static(process.cwd() + "/public"));
-
-var exphbs = require ("express-handlebars");
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+//Require set up handlebars
+var exphbs = require("express-handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
 app.set("view engine", "handlebars");
 
-// mongoose connection
-mongoose.connect("mongodb://localhost/scraped_news");
+//connecting to MongoDB
+//mongoose.connect("mongodb://localhost/scraped_news");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/scraper_news";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
 var db = mongoose.connection;
-// error message or successfully connected to mongoose
-db.on("error", console.error.bind(console, "connection error"));
+db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
-    console.log("Connected to Mongoose!"); 
+  console.log("Connected to Mongoose!");
 });
 
-// set up PORT for 3000
 var routes = require("./controller/controller.js");
 app.use("/", routes);
+//Create localhost port
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
-    console.log("Listening on PORT " + port);
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
 });
-
